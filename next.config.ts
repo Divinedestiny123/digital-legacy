f8457b0b869a27e0f60e4b6853b854cf2d5dcf67
ddboy <divinedestinyokon@gmail.com>
2026-06-23 12:23:32 +0100

    fix: update next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@xenova/transformers"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
